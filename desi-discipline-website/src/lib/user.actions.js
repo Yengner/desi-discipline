538aba6c0139b4279aa62c9e6be226af55ab091d
Yengner <Yengnerb475@gmail.com>
2025-04-12 16:33:49 -0400

    created login and singup page

--- desi-discipline-website/src/lib/user.actions.js
+++ desi-discipline-website/src/lib/user.actions.js
@@ -1,6 +1,25 @@
 
 
+
+
 export async function handleLogin(email, password) {
+
+    try {
+        const supabase = await createSupabaseClient();
+        const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+        if (error) {
+            throw error;
+        }
+
+        return { errorMessage: null }
+
+    } catch (error) {
+        return { errorMessage: getErrorMessage(error) }
+    }
+}
+
+export async function handleSignUp(email, password) {
 
     try {
         const supabase = await createSupabaseClient();

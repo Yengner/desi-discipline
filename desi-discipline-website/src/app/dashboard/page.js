52e3e80f14a5f7661f3a5ffd48a77323429ebff3
Yengner <Yengnerb475@gmail.com>
2025-04-13 09:40:44 -0400

    hello

--- desi-discipline-website/src/app/dashboard/page.js
+++ desi-discipline-website/src/app/dashboard/page.js
@@ -1,9 +1,12 @@
 import DashboardPage from '@/components/dashboard/DashboardPage';
 import { getUserDailyStats } from '@/lib/getUserDailyStats';
 
-const data = await getUserDailyStats();
 
 export default async function Dashboard() {
+
+  const data = await getUserDailyStats();
+
+  
   return (
     <div className="p-28">
         <DashboardPage stats={data}/>
